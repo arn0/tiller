354fcc80c67225024db39c96e6cbc157573ef12c
arn0 <arnoc@xs4all.nl>
2023-09-22 13:18:45 +0200

    Code cleanup

--- main/main.c
+++ main/main.c
@@ -19,18 +19,18 @@ static const char *TAG = "main";
 
 void app_main(void)
 {
-    esp_log_level_set("*", ESP_LOG_WARN);
-    esp_log_level_set("main", ESP_LOG_WARN);
-    esp_log_level_set("wifi station", ESP_LOG_INFO);
+		esp_log_level_set("*", ESP_LOG_WARN);
+		esp_log_level_set("main", ESP_LOG_WARN);
+		esp_log_level_set("wifi station", ESP_LOG_INFO);
 
-    //Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-      ESP_ERROR_CHECK(nvs_flash_erase());
-      ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+		//Initialize NVS
+		esp_err_t ret = nvs_flash_init();
+		if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+			ESP_ERROR_CHECK(nvs_flash_erase());
+			ret = nvs_flash_init();
+		}
+		ESP_ERROR_CHECK(ret);
 
-    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
-    wifi_init_sta();
+		ESP_LOGI(TAG, "Start wifi_init_station()");
+		wifi_init_station();
 }
